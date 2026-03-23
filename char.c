e029b8b90aebe294f2dc35855b1fc39181d4283f
herboh <chandlerfulmer@gmail.com>
2026-03-23 19:42:29 -0400

    lookup table for weird symbols

--- char.c
+++ char.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 
-void print_row(char c) {
-  char symbol[8];
-  char binary[9];
-  int i;
+static const char *ASCII_CONTROL_NAMES[] = {
+    "NUL", "SOH", "STX", "ETX", "EOT", "ENQ",  "ACK", "BEL", "BS",
+    "HT",  "LF",  "VT",  "FF",  "CR",  "SO",   "SI",  "DLE", "DC1",
+    "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",  "CAN", "EM",  "SUB",
+    "ESC", "FS",  "GS",  "RS",  "US",  "SPACE"};
 
-  for (i = 7; i >= 0; i--) {
-    binary[7 - i] = ((c >> i) & 1) ? '1' : '0';
+static void print_binary(unsigned char c) {
+  for (int i = 7; i >= 0; i--) {
+    putchar(((c >> i) & 1u) + '0');
   }
-  binary[8] = '\0';
+}
 
-  if (c == ' ') {
-    snprintf(symbol, sizeof(symbol), "space");
-  } else if (c == '\n') {
-    snprintf(symbol, sizeof(symbol), "\\n");
-  } else if (c == '\t') {
-    snprintf(symbol, sizeof(symbol), "\\t");
-  } else if (c >= 33 && c <= 126) {
-    symbol[0] = c;
+static const char *symbol_label(unsigned char c, char symbol[8]) {
+  if (c <= 32) {
+    return ASCII_CONTROL_NAMES[c];
+  }
+
+  if (c == 127) {
+    return "DEL";
+  }
+
+  if (c <= 126) {
+    symbol[0] = (char)c;
     symbol[1] = '\0';
   } else {
-    snprintf(symbol, sizeof(symbol), "0x%02X", c);
+    snprintf(symbol, 8, "0x%02X", (unsigned)c);
   }
 
-  printf("%-8s %-5u 0x%02X   %s\n", symbol, c, c, binary);
+  return symbol;
+}
+
+static void print_row(unsigned char c) {
+  char symbol[8];
+  unsigned value = c;
+
+  printf("%-8s %-5u 0x%02X   ", symbol_label(c, symbol), value, value);
+  print_binary(c);
+  putchar('\n');
 }
 
 int main(int argc, char *argv[]) {
-  int i;
-
   if (argc > 2) {
-    printf("usage: ./char [text]\n");
+    fprintf(stderr, "usage: %s [text]\n", argv[0]);
     return 1;
   }
 
   printf("%-8s %-5s %-6s %s\n", "symbol", "value", "hex", "binary");
 
   if (argc == 1) {
-    for (i = 0; i <= 127; i++) {
-      print_row((char)i);
+    for (int i = 0; i < 128; i++) {
+      print_row((unsigned char)i);
     }
   } else {
-    for (i = 0; argv[1][i] != '\0'; i++) {
-      print_row((char)argv[1][i]);
+    for (int i = 0; argv[1][i] != '\0'; i++) {
+      print_row((unsigned char)argv[1][i]);
     }
   }
 
